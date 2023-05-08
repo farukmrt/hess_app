b03507f6231aa4c871c7f5fdcdbae1fc523186cf
farukmrt <farukmrt1@gmail.com>
2023-05-08 11:46:12 +0300

    baslangic

---
+++ android/app/src/main/kotlin/com/example/hess_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hess_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+//import 'package:flutter/giderler.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: //ToDo: ana main buraya eklenecek..,
+     ),
+  );
+}
