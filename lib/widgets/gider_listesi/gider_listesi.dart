04c2e8eda2ee72b33894a441da7ff0044191749e
farukmrt <farukmrt1@gmail.com>
2023-05-11 02:40:53 +0300

    grafik_eklenecek_Sorunusz

--- lib/widgets/gider_listesi/gider_listesi.dart
+++ lib/widgets/gider_listesi/gider_listesi.dart
@@ -8,15 +8,41 @@ class giderListesi extends StatelessWidget {
   const giderListesi({
     super.key,
     required this.giderler,
+    required this.onGiderSil,
   });
 
   final List<gider> giderler;
+  final void Function(gider giderCek) onGiderSil;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: giderler.length,
-      itemBuilder: (ctx, index) => giderItem(giderler[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(giderler[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.1),
+          margin: EdgeInsets.symmetric(
+              horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+          child: const Align(
+            alignment: Alignment.centerRight,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.end,
+            child: Icon(
+              Icons.delete,
+              size: 40,
+              color: Colors.white,
+              //row ekle sagina cop kutusu sola kayacak
+            ),
+          ),
+        ),
+        onDismissed: ((direction) {
+          onGiderSil(
+            giderler[index],
+          );
+        }),
+        child: giderItem(giderler[index]),
+      ),
     );
   }
 }
